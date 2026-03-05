46a02e8b9a94404c90e9c9279f5fc62dea5c7e71
Nitin Kumar Singh <101050776+NitinSinghRECB@users.noreply.github.com>
2026-03-05 15:37:59 +0530

    fixx

--- client-react/src/pages/public/Home.jsx
+++ client-react/src/pages/public/Home.jsx
@@ -4,6 +4,7 @@ import TrustBar from '../../components/ui/TrustBar';
 import MenuSection from '../../components/ui/MenuSection';
 import GallerySection from '../../components/ui/GallerySection';
 import AboutSection from '../../components/ui/AboutSection';
+import API_BASE from "../../api";
 
 const Home = () => {
     return (
@@ -26,3 +27,13 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+
+
+useEffect(() => {
+  fetch(`${API_BASE}/menu`)
+    .then(res => res.json())
+    .then(data => setMenu(data));
+}, []);
